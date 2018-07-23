2c09054a177537f83b9c02856eef4ddd2b19f771
Tim Stoddard <tim.stoddard2@gmail.com>
2018-07-23 13:34:15 -0700

    set up initial server api

---
+++ server/Models/Address.cs
@@ -0,0 +1,12 @@
+namespace ParkBnb.Models
+{
+    public class Address
+    {
+        public long Id { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ server/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ParkBnb.Models;
+
+namespace ParkBnb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ParkingSpotContext>(opt => opt.UseInMemoryDatabase("ParkingSpot"));
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
